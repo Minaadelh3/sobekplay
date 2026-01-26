d39979405354a7beef08d5909b42c5783022e563
Minaadelh3 <157021573+Minaadelh3@users.noreply.github.com>
2026-01-26 19:19:40 +0200

    1

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sobek.play',
+  appName: 'Sobek Play',
+  webDir: 'dist'
+};
+
+export default config;
