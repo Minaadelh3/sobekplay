94f9c7fb31dce2e497a24ddc9d7007e43ee80bc3
Minaadelh3 <157021573+Minaadelh3@users.noreply.github.com>
2026-02-09 21:56:55 +0200

    Update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.3hqjf2183dk"
+    "revision": "0.mers6irm7io"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
